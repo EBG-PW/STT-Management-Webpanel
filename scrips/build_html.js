d67f45b0bbc2ccc82cee05a4ec7075402f96f80b
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2020-12-04 02:57:58 +0100

    Script to buld HTML

--- scrips/build_html.js
+++ scrips/build_html.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
 const fs = require("fs");
 
-function CreateHTML(){
-    const Vorlage = fs.readFileSync('../template/index.html').toString();
+function CreateHTML(htmlname){
+    const Vorlage = fs.readFileSync(`./template/${htmlname}.html`).toString();
     
     //Insert IP and PORT
     var FertigHTML = Vorlage.split("REPLACE_THIS_WITH_BACKEND_IP").join(process.env.IP)
     FertigHTML = FertigHTML.split("REPLACE_THIS_WITH_BACKEND_PORT").join(process.env.PORT)
-    //Insert Ping Parameter
-    FertigHTML = FertigHTML.split("REPLACE_THIS_WITH_PINGINFO_GREEN").join(process.env.PingGreen)
-    FertigHTML = FertigHTML.split("REPLACE_THIS_WITH_PINGINFO_YELLOW").join(process.env.PingYellow)
-    FertigHTML = FertigHTML.split("REPLACE_THIS_WITH_PINGINFO_ORANGE").join(process.env.PingOrange)
 
-    console.log(`HTML was build:\nIP: ${process.env.IP}\nPort: ${process.env.PORT}\nPingTimes:\nGrün: Von 0 bis ${process.env.PingGreen}\nGelb: Von ${process.env.PingGreen} bis ${process.env.PingYellow}\nGelb: Von ${process.env.PingYellow} bis ${process.env.PingOrange}\nRot: Über ${process.env.PingOrange}`)
+    console.log(`${htmlname} was build:\nIP: ${process.env.IP}\nPort: ${process.env.PORT}`)
 
-    fs.writeFile("../src/Web/index.html", FertigHTML, (err) => {if (err) console.log(err);
+    fs.writeFile(`./src/web/${htmlname}.html`, FertigHTML, (err) => {if (err) console.log(err);
 
 	});
 }
 
-CreateHTML()
+CreateHTML("index")
+CreateHTML("Teams")
+CreateHTML("TrustFactor")
