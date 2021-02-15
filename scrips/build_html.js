a29b597e2ecea678ee0c26869a508842bc380bf9
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2021-02-15 23:17:37 +0100

    Added IconURL to .env

--- scrips/build_html.js
+++ scrips/build_html.js
@@ -8,6 +8,8 @@ function CreateHTML(htmlname){
     var FertigHTML = Vorlage.split("REPLACE_THIS_WITH_BACKEND_IP").join(process.env.IP)
     FertigHTML = FertigHTML.split("REPLACE_THIS_WITH_BACKEND_PORT").join(process.env.BuildPort)
     FertigHTML = FertigHTML.split("REPLACE_THIS_WITH_BACKEND_PROTOKOL").join(process.env.HTTP)
+    FertigHTML = FertigHTML.split("REPLACE_THIS_WITH_GEODRAGON_STRANGE_URL_UPDATE_ALLE_ZWEI_WOCHEN_SCHEIßE").join(process.env.IconURL)
+    
 
     console.log(`${htmlname} was build:\nIP: ${process.env.IP}\nPort: ${process.env.PORT}`)
 
