004d75574cb7c55bc371c818660dbe409634d94e
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2021-03-07 17:49:56 +0100

    Updated API Plugin loader

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs');
-
 const router = express.Router();
 
 const plugins = [];
@@ -8,8 +7,7 @@ const Preplugins = [];
 let iimport;
 
 Array.prototype.remove = function () {
-  let what; const a = arguments; let L = a.length; let
-    ax;
+  let what; const a = arguments; let L = a.length; let ax;
   while (L && this.length) {
     what = a[--L];
     while ((ax = this.indexOf(what)) !== -1) {
@@ -35,32 +33,35 @@ fs.readdir('./src/api', (err, filenames) => {
 });
 
 /* Load in all the plugins */
-fs.readdir('./src/api', (err, filenames) => {
+fs.readdir("./src/api", function(err, filenames) {
   filenames.remove('index.js');
-  for (i = 0; i < filenames.length; i++) {
-    if (filenames[i].startsWith('disabled.')) {
-      console.log('[API.Plugins] \x1b[36m[WA]\x1b[0m', `Skipped ${filenames[i].slice(9, filenames[i].length - 3)} because its disabled`);
-    } else if (filenames[i].endsWith('.js')) {
+  for (i = 0; i < filenames.length; i++) { 
+    if(filenames[i].startsWith("disabled.")){
+      console.log('[API.Plugins] \x1b[36m[WA]\x1b[0m',`Skipped ${filenames[i].slice(9, filenames[i].length-3)} because its disabled`)
+    }else if(filenames[i].endsWith(".js")){
       let PluginRequirementsFailed = false;
-      const name = filenames[i].slice(0, filenames[i].length - 3);
+      let name = filenames[i].slice(0, filenames[i].length-3)
       iimport = require(`./${name}`);
-      iimport.PluginRequirements.map((Req) => {
-        if (!Preplugins.includes(Req)) { PluginRequirementsFailed = true; }
-      });
-      if (!PluginRequirementsFailed) {
-        plugins.push({
-          route: `/${name}`, name: iimport.PluginName, version: iimport.PluginVersion, author: iimport.PluginAuthor, docs: iimport.PluginDocs
+      if(typeof(iimport.PluginRequirements) === "undefined" || typeof(iimport.PluginName) === "undefined" || typeof(iimport.PluginVersion) === "undefined" || typeof(iimport.PluginAuthor) === "undefined" || typeof(iimport.PluginDocs) === "undefined" || typeof(iimport.router) === "undefined"){
+        console.log('[API.Plugins] \x1b[31m[ER]\x1b[0m',`Skipped ${filenames[i].slice(0, filenames[i].length-3)} because there are missing exports!`)
+      }else{
+        iimport.PluginRequirements.map(Req => {
+          if(!Preplugins.includes(Req)){PluginRequirementsFailed = true}
         });
-        router.use(`/${name}`, iimport.router);
-        console.log('[API.Plugins] \x1b[32m[OK]\x1b[0m', `Loaded ${filenames[i].slice(0, filenames[i].length - 3)}`);
-      } else {
-        console.log('[API.Plugins] \x1b[31m[ER]\x1b[0m', `Plugin ${filenames[i]} requires following plugins [${iimport.PluginRequirements}] and at least one is missing!`);
+      
+        if(!PluginRequirementsFailed){
+          plugins.push({route: `/${name}`, name: iimport.PluginName, version: iimport.PluginVersion, author: iimport.PluginAuthor, docs: iimport.PluginDocs})
+          router.use(`/${name}`, iimport.router);
+          console.log('[API.Plugins] \x1b[32m[OK]\x1b[0m',`Loaded ${filenames[i].slice(0, filenames[i].length-3)}`)
+        }else{
+          console.log('[API.Plugins] \x1b[31m[ER]\x1b[0m',`Plugin ${filenames[i].slice(0, filenames[i].length-3)}} requires following plugins [${iimport.PluginRequirements}] and at least one is missing!`)
+        }
       }
-    } else {
-      console.log('[API.Plugins] \x1b[31m[ER]\x1b[0m', `Unknown file was skipped ${filenames[i]}`);
+    }else{
+      console.log('[API.Plugins] \x1b[31m[ER]\x1b[0m',`Unknown file was skipped ${filenames[i]}`)
     }
   }
-});
+})
 
 router.get('/', (req, res) => {
   res.json({
